bfe7a15a24364b1e7b74aaa65d6dd177b9bbd92f
THELOSTSOUL <1095533751@qq.com>
2024-01-30 21:21:14 +0800

    clippy

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,7 @@ mod util;
 async fn main() {
     let file_appender = tracing_appender::rolling::never(".", "proxy.log");
     let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
-    let offset = UtcOffset::current_local_offset().expect("should get local offset!");
+    let offset = UtcOffset::current_local_offset().expect("Should get local offset!");
     let timer = OffsetTime::new(
         offset,
         format_description!("[year]-[month]-[day] [hour]:[minute]:[second]"),
@@ -34,39 +34,43 @@ async fn main() {
         .with_max_level(Level::INFO)
         .init();
 
-    let config = Arc::new(Config::load().await.unwrap());
+    let config = Arc::new(Config::load().await.expect("Failed to load config"));
     let root_ca = Arc::new(
         CA::load_or_create(
             config.root_ca_cert_path.as_path(),
             config.root_ca_key_path.as_path(),
         )
         .await
-        .unwrap(),
+        .expect("Failed to load root CA"),
     );
 
-    let listener = TcpListener::bind(config.local_addr().unwrap())
+    let addr = config.local_addr().expect("Parse config address failed");
+    let listener = TcpListener::bind(addr)
         .await
-        .unwrap();
+        .expect("Create listener failed");
     info!("Listening on http://{}", listener.local_addr().unwrap());
 
     loop {
-        let (stream, _) = listener.accept().await.unwrap();
+        match listener.accept().await {
+            Ok((stream, _)) => {
+                let config = config.clone();
+                let root_ca = root_ca.clone();
 
-        let config = config.clone();
-        let root_ca = root_ca.clone();
+                let io = TokioIo::new(stream);
 
-        let io = TokioIo::new(stream);
-
-        tokio::task::spawn(async move {
-            if let Err(err) = ServerBuilder::new()
-                .preserve_header_case(true)
-                .title_case_headers(true)
-                .serve_connection(io, Proxy::new(config, root_ca))
-                .with_upgrades()
-                .await
-            {
-                error!("Failed to serve connection: {:?}", err);
+                tokio::task::spawn(async move {
+                    if let Err(err) = ServerBuilder::new()
+                        .preserve_header_case(true)
+                        .title_case_headers(true)
+                        .serve_connection(io, Proxy::new(config, root_ca))
+                        .with_upgrades()
+                        .await
+                    {
+                        error!("Failed to serve connection: {err:?}");
+                    }
+                });
             }
-        });
+            Err(err) => error!("Failed to accept: {err:?}"),
+        }
     }
 }
